462f99b3c2b13720a1e0cdda48a5dd141879a108
Matt Giger <matt.giger@gmail.com>
2024-04-18 05:41:06 -0700

    Initial commit

---
+++ PNG.ts
@@ -0,0 +1,164 @@
+///
+/// EarthBrowser
+///
+/// Created by Matt Giger on 4/16/2024
+/// Copyright (c) 2024 Geodata Labs
+///
+
+import { deflateSync } from "zlib"
+
+export enum PNGColorType {
+	Greyscale = 0,
+	Truecolour = 2,
+	IndexedColor = 3,
+	GreyscaleWithAlpha = 4,
+	TruecolourWithAlpha = 6,
+}
+
+export enum PNGCompression {
+	NoCompression = 0,
+	BestSpeed = 1,
+	MidCompression = 6,
+	BestCompression = 9,
+}
+
+export type PNGBitDepth = 1 | 2 | 4 | 8 | 16 | 32
+
+export class PNG {
+	public width: number
+	public height: number
+	public bitDepth: PNGBitDepth
+	public colorType: PNGColorType
+
+	constructor(width: number, height: number, bitDepth: PNGBitDepth, colorType: PNGColorType) {
+		this.width = width
+		this.height = height
+		this.bitDepth = bitDepth
+		this.colorType = colorType
+	}
+
+	public async compress(imgData: Uint8Array, compression: PNGCompression = PNGCompression.BestSpeed): Promise<Uint8Array> {
+		const imageDataChunk = new IDAT(this.width, this.height, this.bitDepth)
+		await imageDataChunk.compress(imgData, compression)
+
+		// add whatever chunks you want here right before the IEND chunk
+		const chunks: PNGChunk[] = [
+			new IHDR(this.width, this.height, this.bitDepth, this.colorType),
+			new PLTE(),
+			imageDataChunk,
+			new PNGChunk(0x49454e44), // IEND
+		]
+		const blocks: Uint8Array[] = chunks.map(c => this.chunkData(c))
+		const length = blocks.reduce((acc, block) => acc + block.length, 0)
+
+		const block = new Uint8Array(length + 8)
+		const data = new DataView(block.buffer)
+		data.setUint32(0, 0x89504e47) // PNG magic number
+		data.setUint32(4, 0x0d0a1a0a) // DOS line ending
+
+		let offset = 8
+		for(const b of blocks) {
+			block.set(b, offset)
+			offset += b.length
+		}
+
+		return block
+	}
+
+	private chunkData(chunk: PNGChunk): Uint8Array {
+		const block = new Uint8Array(chunk.length + 12)
+		const data = new DataView(block.buffer)
+		data.setUint32(0, chunk.length)
+		data.setUint32(4, chunk.type)
+
+		block.set(chunk.data, 8)
+
+		const crc = crc32(0, block, chunk.length + 4, 4)
+		data.setUint32(8 + chunk.length, crc)
+		return block
+	}
+}
+
+class PNGChunk {
+	public data = new Uint8Array(0)
+	constructor(public type: number) {}
+	public get length(): number { return this.data.length }
+}
+
+class IHDR extends PNGChunk {
+	constructor(public width: number, public height: number, public bitDepth: PNGBitDepth, public colorType: PNGColorType) {
+		super(0x49484452)
+
+		this.data = new Uint8Array(13)
+		const view = new DataView(this.data.buffer)
+		view.setUint32(0, width)
+		view.setUint32(4, height)
+		view.setUint8(8, bitDepth)
+		view.setUint8(9, colorType)
+		view.setUint8(10, 0) // compression
+		view.setUint8(11, 0) // filter
+		view.setUint8(12, 0) // interlace
+	}
+}
+
+class PLTE extends PNGChunk {
+	constructor() {
+		super(0x504c5445)
+
+		this.data = new Uint8Array(3)
+		this.data[0] = 0	// red
+		this.data[1] = 0	// green
+		this.data[2] = 0	// blue
+	}
+}
+
+class IDAT extends PNGChunk {
+	public width: number
+	public height: number
+	public bitDepth: number
+
+	constructor(width: number, height: number, bitDepth: number) {
+		super(0x49444154)
+
+		this.width = width
+		this.height = height
+		this.bitDepth = bitDepth
+	}
+
+	public async compress(imgData: Uint8Array, compression: PNGCompression): Promise<void> {
+		const filterType = 0	// Add filtering here
+		const byteWidth = Math.ceil(this.width * this.bitDepth / 8)
+		const rowWidth = byteWidth + 1
+		const filterData = new Uint8Array(rowWidth * this.height)
+		for(let i=0; i<this.height; i++) {
+			filterData[i * rowWidth] = filterType
+			filterData.set(imgData.subarray(i * byteWidth, (i + 1) * byteWidth), i * rowWidth + 1)
+		}
+
+        this.data = deflateSync(filterData, {level: compression, windowBits: 15, strategy: 3})
+	}
+}
+
+
+const crcTable = makeTable()
+
+function makeTable(): number[] {
+	const table: number[] = []
+	for (let n = 0; n < 256; n++) {
+		let c = n
+		for (let k = 0; k < 8; k++) {
+			c = ((c & 1) ? (0xEDB88320 ^ (c >>> 1)) : (c >>> 1))
+		}
+		table[n] = c
+	}
+	return table
+}
+
+function crc32(crc: number, buf: Uint8Array, len: number, pos: number): number {
+	const end = pos + len
+	crc ^= -1
+	for (let i = pos; i < end; i++) {
+		crc = (crc >>> 8) ^ crcTable[(crc ^ buf[i]) & 0xFF]
+	}
+	return (crc ^ (-1))
+}
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # PNG
-Simple one file typescript PNG creation class
+
+Simple Typescript PNG creation class. Just paste this class into your code and you're living large.
+
+Usage:
+
+```typescript
+	// Note: PNG spec requires 16 and 32 bit data to be big-endian
+	const imageData: Uint8Buffer =  <raw image data>
+
+	const png = new PNG(tileSize, tileSize, 16, PNGColorType.Greyscale)
+	const pngData = await png.compress(imageData, PNGCompression.BestCompression)
+```
+
+Dependencies:
+
+* zlib
+
